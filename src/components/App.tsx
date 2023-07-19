16860ad0ff27903570ada46c5987d9ad707fe127
larryli <lzhao@atlashxm.com>
2023-07-19 11:13:18 +0800

    Integrate eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { type ReactElement } from 'react';
 
-type AppProps = { num: number };
+interface AppProps { num: number }
 
-const App = ({num}: AppProps) => (<h1>Total Number: {num}</h1>);
+const App = ({ num }: AppProps): ReactElement => (<h1>Total Number: {num}</h1>);
 
 export default App;
