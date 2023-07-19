4404c3db967d2a52e7c38604cf4e5c93e10fb8dc
larryli <lzhao@atlashxm.com>
2023-07-19 13:11:55 +0800

    Fix linting problems

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "../src/index.js"),
-    output: {
-        path: path.join(__dirname, '../dist'),
-        filename: '[name].js',
-        chunkFilename: '[name].chunk.js'
+  entry: path.join(__dirname, "../src/index.js"),
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+    chunkFilename: "[name].chunk.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[t|j]sx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "../src/index.html"),
+      inject: true
+    })
+  ],
+  resolve: {
+    alias: {
+      "@Components": path.join(__dirname, "../src/components")
     },
-    module: {
-        rules: [
-            {
-                test: /\.[t|j]sx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
-    },
-    plugins:[
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname,'../src/index.html'),
-            inject: true
-        })
-    ],
-    resolve: {
-        alias: {
-            '@Components': path.join(__dirname, '../src/components'),
-        },
-        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
-    }
-}
+    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"]
+  }
+};
