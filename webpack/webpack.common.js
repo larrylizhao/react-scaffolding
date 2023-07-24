3ee23778ab9272ea667df245cb840a38fbcb38cb
larryli <lzhao@atlashxm.com>
2023-07-24 11:05:07 +0800

    Update webpack config for alias

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,38 +1,41 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "../src/index.js"),
+  entry: path.join(__dirname, '../src/index.js'),
   output: {
-    path: path.join(__dirname, "../dist"),
-    filename: "[name].js",
-    chunkFilename: "[name].chunk.js"
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js'
   },
   module: {
     rules: [
       {
         test: /\.[t|j]sx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "../src/index.html"),
+      template: path.join(__dirname, '../src/index.html'),
       inject: true
     })
   ],
   resolve: {
     alias: {
-      "@Components": path.join(__dirname, "../src/components")
+      '@api': path.join(__dirname, '../src/api'),
+      '@components': path.join(__dirname, '../src/components'),
+      '@app': path.join(__dirname, '../src/app'),
+      '@features': path.join(__dirname, '../src/features')
     },
-    extensions: [".tsx", ".ts", ".js", ".jsx", ".json"]
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json']
   }
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,11 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 3000,
     hot: true
   },
-  devtool: "eval-source-map"
+  devtool: 'eval-source-map'
 });
