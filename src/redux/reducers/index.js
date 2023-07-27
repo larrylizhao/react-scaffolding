bdfeeaf40d50508764d229bdaa507207f834455e
larryli <lzhao@atlashxm.com>
2023-07-27 14:14:33 +0800

    feat: Integrate RTK query into redux store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,15 +3,18 @@
  */
 import counterReducer from './counterSlice';
 import userReducer from './userSlice';
+import { baseApi } from '@Api/base';
 
 /*
   Actions
  */
 export * from './counterSlice';
+export * from './userSlice';
 
 const reducer = {
   counter: counterReducer,
-  user: userReducer
+  user: userReducer,
+  [baseApi.reducerPath]: baseApi.reducer
 };
 
 export default reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, type ThunkDispatch } from '@reduxjs/toolkit';
 import reducer from './reducers';
+import { baseApi } from '@Api/base';
+import { setupListeners } from '@reduxjs/toolkit/dist/query/react';
 
 export const store = configureStore({
   reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
   preloadedState: { counter: { value: 1 } },
   devTools:
     process.env.NODE_ENV !== 'production'
@@ -14,3 +18,5 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+setupListeners(store.dispatch as ThunkDispatch<any, any, any>);
