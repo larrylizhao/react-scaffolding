5430f594e4f367041bd2b571d98a74133a612acc
larryli <lzhao@atlashxm.com>
2023-07-31 17:45:12 +0800

    feat: Change options for base api

--- src/api/base.ts
+++ src/api/base.ts
@@ -5,8 +5,7 @@ export const baseApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://jsonplaceholder.typicode.com'
   }),
-  // 缓存，默认时间是秒，默认时长60秒
-  keepUnusedDataFor: 5 * 60,
-  refetchOnMountOrArgChange: 30 * 60,
+  keepUnusedDataFor: 5,
+  refetchOnMountOrArgChange: 2,
   endpoints: () => ({})
 });
