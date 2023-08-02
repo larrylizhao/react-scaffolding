2a35198a5f2f32120ff40130b72858ae99053926
larryli <lzhao@atlashxm.com>
2023-08-02 15:43:57 +0800

    test: Update test for login page

--- src/pages/__tests__/Login.test.js
+++ src/pages/__tests__/Login.test.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import Login from '@Pages/Login';
 import { screen } from '@testing-library/react';
-import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { renderWithProviders } from '@Utils/test-utils';
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
+  useNavigate: jest.fn(() => () => {}),
   useLocation: jest
     .fn()
     .mockReturnValue({ state: { form: { pathname: '/dashboard' } } })
@@ -14,6 +15,17 @@ describe('Login', () => {
   it('should render Login page correctly', () => {
     // Since we're using redux inside the Login component we need to render it with Provider to provide the store
     renderWithProviders(<Login />);
+    expect(screen.getByRole('button', { name: /login/i })).toBeVisible();
+  });
+
+  it('should log the user in', async () => {
+    const user = userEvent.setup();
+    renderWithProviders(<Login />);
+    await user.type(
+      screen.getByRole('textbox', { name: /username/i }),
+      'testing'
+    );
+    await user.click(screen.getByRole('button', { name: /login/i }));
     expect(screen.getByRole('button', { name: /login/i })).toBeVisible();
   });
 });
