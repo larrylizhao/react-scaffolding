d0a7cbb5cb82c5ce9468f3cd44ba43bbd35d190a
larryli <lzhao@atlashxm.com>
2023-08-02 11:10:53 +0800

    Handle css for jest

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,10 @@ module.exports = {
     '@Api(.*)$': '<rootDir>/src/api$1',
     '@Pages(.*)$': '<rootDir>/src/pages$1',
     '@Redux(.*)$': '<rootDir>/src/redux$1',
-    '@Components(.*)$': '<rootDir>/src/components$1'
+    '@Components(.*)$': '<rootDir>/src/components$1',
+    '@Utils(.*)$': '<rootDir>/src/utils$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css)$': '<rootDir>/__mocks__/styleMock.js'
   }
 };
